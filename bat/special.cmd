96196204f422a71991eac4895bdc5862ba39cf95
Summer Fang <summerfang@gmail.com>
2020-10-30 11:35:52 -0700

    Study of batch

---
+++ bat/special.cmd
@@ -0,0 +1,6 @@
+@echo off
+echo.
+echo Error: JAVA_HOME not found in your environment. >&2
+echo Please set the JAVA_HOME variable in your environment to match the >&2
+echo location of your Java installation. >&2
+echo.
