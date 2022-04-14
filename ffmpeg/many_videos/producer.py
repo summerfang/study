a72f6450d7ee4176d0d3986f4bc5e23648242d9e
Summer Fang <summerfang@gmail.com>
2022-04-14 00:09:20 -0700

    learn(git): stash-noid

--- ffmpeg/many_videos/producer.py
+++ ffmpeg/many_videos/producer.py
@@ -38,3 +38,5 @@ if __name__ == "__main__":
     str_output2ffplay = '-map "[video]" -map "[audio]" -preset ultrafast -tune zerolatency -crf 28 -g 60 -c:a aac -f matroska - | ffplay -'
     cmd_line = cmd_line + filter_complex_string + '"' + bg_string + str_video + str_audio + '" ' + str_output2ffplay
     print(cmd_line)
+
+# Good work!
