5ae541d3eb38500ba75e020194408da32e860261
Summer Fang <summerfang@gmail.com>
2022-04-19 19:16:55 -0700

    remove comment

--- ffmpeg/many_videos/producerex1.py
+++ ffmpeg/many_videos/producerex1.py
@@ -13,17 +13,17 @@ if __name__ == "__main__":
     layer1 = Layer(s1, width=360, height=600, x=120)
     vl.append(layer1)
 
-    # s2 = ffmpeg.input("spk2.mp4")
-    # layer2 = Layer(s2, width=360, height=600, x=120)
-    # vl.append(layer2)
-    # s3 = ffmpeg.input("spk3.mp4")
-    # layer3 = Layer(s3, width=360,height=600, x=120)
+    s2 = ffmpeg.input("spk2.mp4")
+    layer2 = Layer(s2, width=360, height=600, x=120)
+    vl.append(layer2)
+    s3 = ffmpeg.input("spk3.mp4")
+    layer3 = Layer(s3, width=360,height=600, x=120)
  
-    # vl.append(layer3)
+    vl.append(layer3)
 
-    # s4 = ffmpeg.input("spk4.mp4")
-    # layer4 = Layer(s4, width=360,height=600, x=120)
-    # vl.append(layer4)
+    s4 = ffmpeg.input("spk4.mp4")
+    layer4 = Layer(s4, width=360,height=600, x=120)
+    vl.append(layer4)
 
     logo = ffmpeg.input("logo.mp4")
     layer_logo = Layer(logo, width=180,height=100, x=120, position_x=1280 - 180 - 10, position_y= 25, layer_type=Layer.LOGO)
