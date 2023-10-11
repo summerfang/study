899cd10231bd1df0c4e48ad13dc6333ca960ac38
Summer Fang <summerfang@gmail.com>
2023-10-11 11:30:22 -0700

    Add .env to store OPENAI_API_KEY

--- python/openai/callapi.py
+++ python/openai/callapi.py
@@ -1,7 +1,7 @@
 import os
 import openai
 
-openai.api_key = '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'
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 response = openai.Completion.create(
   model="text-davinci-003",
