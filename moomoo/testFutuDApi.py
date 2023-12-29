1f740c19c6d4f7b3105cc8f6433f5a75f0164148
Summer Fang <summerfang@gmail.com>
2023-12-28 17:32:28 -0800

    Study moomoo api

--- moomoo/testFutuDApi.py
+++ moomoo/testFutuDApi.py
@@ -1,11 +1,11 @@
-from futu import *
+from moomoo import *
 
 quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)  # Create quote object
-print(quote_ctx.get_market_snapshot('HK.00700'))  # Get market snapshot for HK.00700
+print(quote_ctx.get_market_snapshot('US.AAPL'))  # Get market snapshot for HK.00700
 quote_ctx.close() # Close object to prevent the number of connextions from running out
 
 
 trd_ctx = OpenSecTradeContext(host='127.0.0.1', port=11111)  # Create trade object
-print(trd_ctx.place_order(price=500.0, qty=100, code="HK.00700", trd_side=TrdSide.BUY, trd_env=TrdEnv.SIMULATE))  # Placing an order through paper trading account (It is nessary to unlock trade by trading password for placing orders in the real environment.)
+print(trd_ctx.place_order(price=500.0, qty=100, code="US.AAPL", trd_side=TrdSide.BUY, trd_env=TrdEnv.SIMULATE))  # Placing an order through paper trading account (It is nessary to unlock trade by trading password for placing orders in the real environment.)
 
 trd_ctx.close()  # Close object to prevent the number of connextions from running out
