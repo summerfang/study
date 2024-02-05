87fc384798a1f68ec8991b813cc5e06e34bc0345
Summer Fang <summerfang@gmail.com>
2024-02-05 15:16:23 -0800

    This codes learn how to handle audio

--- python/e2c_video/v2a.py
+++ python/e2c_video/v2a.py
@@ -1,8 +1,10 @@
 import subprocess
 
 def extract_audio(input_file, output_file):
-    command = ['ffmpeg', '-i', input_file, '-vn', '-acodec', 'copy', output_file]
-    subprocess.run(command)
+    # command = ['ffmpeg', '-i', input_file, '-c:a libmp3lame -y', output_file]
+    command = ['ffmpeg', '-i', input_file, '-c:a pcm_s16le', '-ar 44100', output_file]
+    result = subprocess.run(command)
+    print("Command Line:", ' '.join(result.args))
 
 # Replace 'input_video.mp4' and 'output_audio.mp3' with your actual file names
-extract_audio('input_video.mp4', 'output_audio.mp3')
+extract_audio('input_video.mp4', 'output_audio.wav')
