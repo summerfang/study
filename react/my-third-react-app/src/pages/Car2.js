6c2486494321800842a634f1166dd2b1a00738bd
Summer Fang <summerfang@gmail.com>
2024-03-04 18:07:50 -0800

    learn react

--- react/my-third-react-app/src/pages/Car2.js
+++ react/my-third-react-app/src/pages/Car2.js
@@ -9,9 +9,21 @@ const Car2 = () => {
         make: 'Model S'
     });
 
+    const updateColor = () => { 
+        setCar(previousState => {
+            return {
+                ...previousState,
+                color: 'blue'
+            };
+        });
+    };
+
     return (
         <>
             <h1>My car is a {car.color} {car.model} which is made in {car.year} {car.make}</h1>
+            <button 
+                onClick={updateColor}
+            >Blue</button>    
         </>
     );
 
