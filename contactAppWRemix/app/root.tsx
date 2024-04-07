e0040e7e22251cc67ffbdb840e6f214928420697
summerfang <summerfang@gmail.com>
2024-04-07 14:35:45 -0700

    learning remix and react router

---
+++ contactAppWRemix/app/root.tsx
@@ -0,0 +1,65 @@
+import type { LinksFunction } from "@remix-run/node";
+
+import appStyleHref from "./app.css?url";
+
+export const links: LinksFunction = () => [
+  {rel : "stylesheet", href: appStyleHref},
+];
+
+import {
+  Form,
+  Link,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div id="sidebar">
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form id="search-form" role="search">
+              <input
+                id="q"
+                aria-label="Search contacts"
+                placeholder="Search"
+                type="search"
+                name="q"
+              />
+              <div id="search-spinner" aria-hidden hidden={true} />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+            <ul>
+              <li>
+              <Link to={`/contacts/1`}>Your Name</Link>
+              </li>
+              <li>
+              <Link to={`/contacts/2`}>Your Friend</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div id="detail">
+          <Outlet />
+        </div>
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
