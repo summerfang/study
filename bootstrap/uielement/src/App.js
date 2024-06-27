f1063b31fc66f9fc0e0fb4a7a109f4c50426db2e
Summer Fang <summerfang@gmail.com>
2024-06-27 15:35:03 -0700

    example of using react-bootstrap

---
+++ bootstrap/uielement/src/App.js
@@ -0,0 +1,29 @@
+// import logo from './logo.svg';
+import './App.css';
+import 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import BasicExample from './BasicExample';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BasicExample />        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
